f8a2fc4d74827ecf51382b5160299b3df3713cd9
peeaaccee <wlgp8958@naver.com>
2023-10-25 00:23:53 +0900

    feat(BallGenerator): 컴퓨터의 랜덤볼 생성 (1~9까지의 중복 없는 3자리수)

--- src/main/java/baseball/domain/model/BallGenerator.java
+++ src/main/java/baseball/domain/model/BallGenerator.java
@@ -1,4 +1,42 @@
 package baseball.domain.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BallGenerator {
+    private static final int MAX_SIZE = 3;
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 9;
+
+    public BallGenerator() {
+    }
+
+    private static class BallHolder {
+        private static final BallGenerator INSTANCE = new BallGenerator();
+    }
+
+    public static BallGenerator getInstance() {
+        return BallHolder.INSTANCE;
+    }
+
+    public String createRandomBall() {
+        return String.valueOf(Randoms.pickNumberInRange(START_NUMBER, END_NUMBER));
+    }
+
+    public List<String> makeComputerBalls() {
+        List<String> balls = new ArrayList<>();
+        while (balls.size() < MAX_SIZE) {
+            checkBallDuplication(balls);
+        }
+        return balls;
+    }
+
+    private void checkBallDuplication(List<String> balls) {
+        String randomball = createRandomBall();
+        if (!balls.contains(randomball)) {
+            balls.add(randomball);
+        }
+    }
 }
