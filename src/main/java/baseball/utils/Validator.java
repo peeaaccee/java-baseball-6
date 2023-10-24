4bba0749f05cd02926cf8c1494196a3693a92483
peeaaccee <wlgp8958@naver.com>
2023-10-25 05:43:26 +0900

    feat(Validator): 볼 숫자의 크기가 3인지 검사

--- src/main/java/baseball/utils/Validator.java
+++ src/main/java/baseball/utils/Validator.java
@@ -1,4 +1,24 @@
 package baseball.utils;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class Validator {
+    private static final List<String> STANDARD_NUMBER = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9");
+    private static final int MAX_SIZE = 3;
+
+    private Validator() {
+    }
+
+    public static void checkBallInput(final String ballInput) {
+        checkBallNumberLength(ballInput);
+    }
+
+    private static void checkBallNumberLength(final String ballInput) {
+        if (MAX_SIZE != ballInput.length()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
